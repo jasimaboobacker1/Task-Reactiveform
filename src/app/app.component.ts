2afe3420811d1171df104e3689408b4f0bfc04aa
unknown <jasimaboobacker1>
2024-04-05 14:32:55 +0530

    message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,54 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FormsModule,CommonModule,ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'reactive-form';
+  formSubmitted = false;
+  constructor(private _fb:FormBuilder){}
+
+  form!:FormGroup;
+  ngOnInit(): void {
+    this.form = this._fb.group({
+      bookname: ['', [Validators.required]],
+      author: ['',Validators.required],
+      publication: ['',Validators.required],
+      price: ['',Validators.required],
+      genre: ['',Validators.required],
+      publishedDate: ['',Validators.required],
+      country: ['',Validators.required],
+      description: ['',Validators.required],
+    });
+  }
+  
+  onSubmit() {
+    if (this.form.valid) {
+      
+      const formValue = this.form.value;
+      const alertMessage = `
+        Book Name: ${formValue.bookname}
+        Author: ${formValue.author}
+        Publication: ${formValue.publication}
+        Price: ${formValue.price}
+        Genre: ${formValue.genre}
+        Published Date: ${formValue.publishedDate}
+        Country: ${formValue.country}
+        Description: ${formValue.description}
+      `;
+      alert(alertMessage);
+    } else {
+     
+      this.form.markAllAsTouched();
+    }
+  }
+ 
 }
+
